de62ab52239e3083a7e4e82a7f68b7ec2b987c0a
Trey Hoover <treyhoover@gmail.com>
2017-05-09 22:03:12 -0400

    Simplify innerSize utils

--- src/utils/innerSize.js
+++ src/utils/innerSize.js
@@ -1,15 +1,17 @@
 // Calculate height without padding.
 export function innerHeight(el) {
     const style = window.getComputedStyle(el, null);
-    return el.clientHeight -
-        parseInt(style.getPropertyValue('padding-top'), 10) -
-        parseInt(style.getPropertyValue('padding-bottom'), 10);
+    const paddingTop = parseInt(style.getPropertyValue('padding-top'), 10);
+    const paddingBottom = parseInt(style.getPropertyValue('padding-bottom'), 10);
+
+    return el.clientHeight - paddingTop - paddingBottom;
 }
 
 // Calculate width without padding.
 export function innerWidth(el) {
     const style = window.getComputedStyle(el, null);
-    return el.clientWidth -
-        parseInt(style.getPropertyValue('padding-left'), 10) -
-        parseInt(style.getPropertyValue('padding-right'), 10);
+    const paddingLeft = parseInt(style.getPropertyValue('padding-left'), 10);
+    const paddingRight = parseInt(style.getPropertyValue('padding-right'), 10);
+
+    return el.clientWidth - paddingLeft - paddingRight;
 }
